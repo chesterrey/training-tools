8ddeec640860023e537e4b871313aa019c77ff21
Chester Tan <rey.tan@obf.ateneo.edu>
2024-02-23 13:25:40 +0800

    updated title and description

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,16 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Collection of training tools <br className="hidden sm:inline" />
+          to assist you in your health and fitness goals.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Interactive apps for creating personalized training plans, offering
+          options for exercise selection, program duration, and routine
+          frequency.
         </p>
       </div>
       <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
         <Link
           target="_blank"
           rel="noreferrer"
